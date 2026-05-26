8292ef752475a08114ae81700cb17f7adef2b784
Omeryildiz08 <omer.yildiz27@hotmail.com>
2026-05-26 13:58:13 +0300

    feat(MergeAnimController) : DOTween for merge animation

--- Assets/Scripts/MergeAnimationController.cs
+++ Assets/Scripts/MergeAnimationController.cs
@@ -2,9 +2,147 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using System;
 
 public class MergeAnimationController : MonoBehaviour
 {
-   [Header("Merge Movement")]
-   [SerializeField] private float mergeMoveDuration = 0.22f;
+    [Header("Merge Movement")]
+    [SerializeField] private float mergeMoveDuration = 0.22f;
+    [SerializeField] private float mergeShrinkScale = 0.05f;
+    [SerializeField] private Ease mergeMoveEase = Ease.InBack;
+    [SerializeField] private Ease mergeScaleEase = Ease.InBack;
+    [SerializeField] private bool rotateDuringMerge = true;
+    [SerializeField] private float mergeRotationAmount = 180f;
+
+    [Header("New Object Pop")]
+    [SerializeField] private float newObjectPopDuration = 0.16f;
+    [SerializeField] private float newObjectSettleDuration = 0.08f;
+    [SerializeField] private float popOvershootScale = 1.15f;
+    [SerializeField] private Ease popEase = Ease.OutBack;
+    [SerializeField] private Ease settleEase = Ease.OutQuad;
+
+    [Header("VFX")]
+    [SerializeField] private GameObject mergeVFXPrefab;
+    [SerializeField] private float mergeVFXLifetime = 2f;
+
+    [Header("SFX")]
+    [SerializeField] private AudioSource audioSource;
+    [SerializeField] private AudioClip mergeSoundClip;
+    [SerializeField] private bool randomizePitch = true;
+    [SerializeField] private Vector2 pitchRange = new Vector2(0.95f, 1.08f);
+
+    public Tween PlayGatherAnimation(IReadOnlyList<MergeableObject> mergeObjects, Vector3 mergeCenterWorldPos, Action onComplete)
+    {
+        if (mergeObjects == null || mergeObjects.Count == 0)
+        {
+            onComplete?.Invoke();
+            return null;
+        }
+
+        Sequence sequence = DOTween.Sequence();
+
+        for (int i = 0; i < mergeObjects.Count; i++)
+        {
+            MergeableObject obj = mergeObjects[i];
+
+            if (obj == null)
+            {
+                continue;
+            }
+
+            Transform objTransform = obj.transform;
+
+            Vector3 targetScale = objTransform.localScale * mergeShrinkScale;
+
+            sequence.Join(
+                objTransform.DOMove(mergeCenterWorldPos, mergeMoveDuration)
+                .SetEase(mergeMoveEase)
+            );
+
+            if (rotateDuringMerge)
+            {
+                sequence.Join(
+                    objTransform
+                        .DORotate(
+                            new Vector3(0f, mergeRotationAmount, 0f),
+                            mergeMoveDuration,
+                            RotateMode.LocalAxisAdd)
+                        .SetEase(Ease.InOutSine)
+                );
+            }
+        }
+        sequence.OnComplete(() =>
+      {
+          PlayMergeFeedback(mergeCenterWorldPos);
+          onComplete?.Invoke();
+      });
+
+        return sequence;
+    }
+
+    public Tween PlayNewObjectPopAnimation(Transform newObjectTransform, Action onComplete)
+    {
+        if (newObjectTransform == null)
+        {
+            onComplete?.Invoke();
+            return null;
+        }
+        Vector3 finalScale = newObjectTransform.localScale;
+        Vector3 overshootScale = finalScale * popOvershootScale;
+
+        newObjectTransform.localScale = Vector3.zero;
+        Sequence sequence = DOTween.Sequence();
+
+        sequence.Append(
+           newObjectTransform
+               .DOScale(overshootScale, newObjectPopDuration)
+               .SetEase(popEase)
+       );
+
+        sequence.Append(
+           newObjectTransform
+               .DOScale(finalScale, newObjectSettleDuration)
+               .SetEase(settleEase)
+       );
+
+        sequence.OnComplete(() =>
+        {
+            onComplete?.Invoke();
+        });
+
+        return sequence;
+    }
+
+    private void PlayMergeFeedback(Vector3 position)
+    {
+        SpawnMergeVFX(position);
+        PlayMergeSFX();
+    }
+
+    private void SpawnMergeVFX(Vector3 position)
+    {
+        if (mergeVFXPrefab = null)
+        {
+            return;
+        }
+        GameObject vfx = Instantiate(mergeVFXPrefab, position, Quaternion.identity);
+        Destroy(vfx, mergeVFXLifetime);
+    }
+
+    private void PlayMergeSFX()
+    {
+        if (audioSource == null || mergeSoundClip == null)
+        {
+            return;
+        }
+        float originalPitch = audioSource.pitch;
+
+        if (randomizePitch)
+        {
+            audioSource.pitch = UnityEngine.Random.Range(pitchRange.x, pitchRange.y);
+        }
+
+        audioSource.PlayOneShot(mergeSoundClip);
+        audioSource.pitch = originalPitch;
+    }
 }
