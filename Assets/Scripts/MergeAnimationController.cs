970e05ce8e113efae4262b2e1185b9537d1ddcd3
Omeryildiz08 <omer.yildiz27@hotmail.com>
2026-05-26 12:52:36 +0300

    dotween setup

---
+++ Assets/Scripts/MergeAnimationController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class MergeAnimationController : MonoBehaviour
+{
+   [Header("Merge Movement")]
+   [SerializeField] private float mergeMoveDuration = 0.22f;
+}
