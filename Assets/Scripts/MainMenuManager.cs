29ebc595d7a2d153b7a88b6f872cd1a23e10137c
Omeryildiz08 <omer.yildiz27@hotmail.com>
2026-05-31 23:24:39 +0300

    main menu update

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,19 +1,69 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    [Header("Scene")]
     public string baseSceneName = "BaseScene";
-   public void OnStartButtonClicked()
+
+    [Header("Panels")]
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject settingsPanel;
+
+    private void Start()
+    {
+        Time.timeScale = 1f;
+
+        if (mainPanel != null)
+        {
+            mainPanel.SetActive(true);
+        }
+
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+        }
+    }
+
+    public void OnStartButtonClicked()
     {
         SceneManager.LoadScene(baseSceneName);
+    }
+
+    public void OnSettingsButtonClicked()
+    {
+        if (mainPanel != null)
+        {
+            mainPanel.SetActive(false);
+        }
+
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(true);
+        }
+    }
+
+    public void OnSettingsBackButtonClicked()
+    {
+        if (settingsPanel != null)
+        {
+            settingsPanel.SetActive(false);
+        }
+
+        if (mainPanel != null)
+        {
+            mainPanel.SetActive(true);
+        }
     }
 
     public void OnExitButtonClicked()
     {
         Debug.Log("Oyundan çıkılıyor...");
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
         Application.Quit();
+#endif
     }
 }
